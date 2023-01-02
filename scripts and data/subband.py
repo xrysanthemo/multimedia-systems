8f73e93c4b6da42ecdec704a536bc3c288cf5ed3
xrysanthemo <chrysaepiskopou@gmail.com>
2023-01-02 13:57:43 +0200

    refactoring

--- scripts and data/subband.py
+++ scripts and data/subband.py
@@ -1,9 +1,10 @@
-
+import math
 import numpy as np
 from mp3 import make_mp3_analysisfb, make_mp3_synthesisfb
 from frame import frame_sub_analysis, frame_sub_synthesis
 from matplotlib import pyplot as plt
 from scipy.io import wavfile
+from nothing import donothing, idonothing
 
 
 
@@ -21,11 +22,6 @@ h = get_impulse_response().reshape(512,)
 H = make_mp3_analysisfb(h, M)
 G = make_mp3_synthesisfb(h, M)
 
-#Transfer function creation
-
-# for i in range(0,M):
-#     H_f = np.fft.fft(H[:,i])
-#     print(H_f)
 
 #opening myfile.wav
 sr, data = wavfile.read('myfile.wav')
@@ -33,17 +29,17 @@ sr, data = wavfile.read('myfile.wav')
 ts = 1.0/sr
 Y = frame_sub_analysis(data, H, N) #xbuff is the input signal
 Z = frame_sub_synthesis(Y, G)
-print(Z)
 
-def get_frame(h, N):
-    Y = frame_sub_analysis(data, h, N)
-    Z = frame_sub_synthesis(Y, G) #Z is the frame
-    return Z
 
-N = len(H)
-n = np.arange(N)
-T = N/sr
-f = n / T
+# def get_frame(h, N):
+#     Y = frame_sub_analysis(data, h, N)
+#     Z = frame_sub_synthesis(Y, G) #Z is the frame
+#     return Z
+#na thumithw giati avta
+ #giati?/
+l = np.arange(L)
+T = L/sr
+f = l / T
 
 plt.figure(figsize=(15,4.8))
 plt.xticks(np.arange(sr, step=4000))
@@ -56,7 +52,7 @@ plt.ylabel('Μέτρο (dB)') #Τίτλος στον άξονα y
 plt.title('Μέτρο συναρτήσεων μεταφοράς (Hz)') #Τίτλος του διαγράμματος
 plt.show()
 
-
+#TODO: Plot function, argument:scale, returning db
 #TODO: Implement a good colormapping
 #TODO: Check if plot is right to plot 2 highs in each filter
 #TODO:Automatically define figsize
@@ -78,4 +74,39 @@ plt.show()
 
 #4th task
 def codec0(wavin, h, M, N):
-    return xhat, Y tot
+    #Διαβάζω το αρχείο .wav
+    sr, data = wavfile.read(wavin)
+    #Κατασκευάζω τα φίλτρα ανάλυσης και σύνθεσης
+    H = make_mp3_analysisfb(h, M)
+    G = make_mp3_synthesisfb(h, M)
+    #Δημιουργώ το xhat
+    xhat = []
+    # sampling interval
+    ts = 1.0 / sr
+    L = len(h) #512, M=21 N=36
+    Y_tot = np.zeros([len(data), M])
+    #Y_tot = []
+    lower = 0
+    for i in range(0,math.ceil(len(data))):
+        # upper = (i + 1) * (M * (N-1) + L)
+        upper = (i+1) * M * N
+        samples = data[lower:upper]
+        if lower + ((N-1)*M + L)  < len(data):
+            buffer = data[lower: lower + ((N-1)*M + L)]
+        else:
+            buffer = data[lower:len(data)]
+        lower = upper + 1
+        #Frame Y mesw analysis se bands
+        Y = frame_sub_analysis(buffer, H, N)
+        Yc = donothing(Y)
+        bound1 = i * N
+        bound2 = (i +1) * N
+        Y_tot[bound1:bound2, :] = Yc
+        Yh = idonothing(Yc)
+        Z = frame_sub_synthesis(Yh, G)
+        xhat.append(Z)
+    #return xhat, Y_tot
+    #TODO: na vrw megethos tou Ytot
+    return Y_tot
+y = codec0('myfile.wav', h, M, N)
+print(y)
