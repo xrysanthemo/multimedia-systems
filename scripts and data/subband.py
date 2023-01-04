655a5032d8f21541cb6956f845f5ab88b4c2c7ab
xrysanthemo <chrysaepiskopou@gmail.com>
2023-01-04 15:52:32 +0200

    codec & snr

--- scripts and data/subband.py
+++ scripts and data/subband.py
@@ -1,10 +1,13 @@
 import math
 import numpy as np
+from numpy import average
+from math import log10
 from mp3 import make_mp3_analysisfb, make_mp3_synthesisfb
 from frame import frame_sub_analysis, frame_sub_synthesis
 from matplotlib import pyplot as plt
 from scipy.io import wavfile
 from nothing import donothing, idonothing
+from scipy import stats
 
 
 
@@ -25,22 +28,25 @@ G = make_mp3_synthesisfb(h, M)
 #4th task
 def codec0(wavin, h, M, N):
     #Διαβάζω το αρχείο .wav
-    sr, data = wavfile.read(wavin)
+    sr, data = wavfile.read(wavin) #514944
     #Κατασκευάζω τα φίλτρα ανάλυσης και σύνθεσης
     H = make_mp3_analysisfb(h, M)
     G = make_mp3_synthesisfb(h, M)
     #Δημιουργώ την παράμετρο για το l
     L = len(h)  # 512
-    iters = math.ceil(len(data)/(M*N))
-    #Αρχικοποιώ Y_tot, xhat
-    Y_tot = np.zeros((N*iters , M))
-    xhat = np.zeros((N*M*iters, ))
-    for i in range(0,446): #to kanw mia fora tha to kanw expand meta
+    buffer_size = (N - 1) * M + L
+    #Υλοποιώ padding του σήματός μου για να μπορέσει να αναλυθεί σε subband
+    padding = np.zeros((L - M,))
+    iters = math.ceil(len(data) / (M * N))
+    # Αρχικοποιώ Y_tot, xhat
+    Y_tot = np.zeros((N * iters, M))
+    xhat = np.zeros(data.shape)
+    data = np.append(data,padding)
+
+    for i in range(0,iters): #to kanw mia fora tha to kanw expand meta
     #Διαβάζω το buffer: (N − 1)M + L
-        buffer_size =  (N-1) * M +L
-        buffer_samples = data[(i * buffer_size):((i + 1) * buffer_size)]
     #Διαβάζω τα δείγματά μου: M*N
-        original_samples = data[(i*M*N):(i+1)*M*N]
+        # original_samples = data[(i*M*N):(i+1)*M*N]
         buffer_samples = data[(i*M*N):(i*M*N)+buffer_size]
     #Frame Sub Analysis sto buffer mou
         Y = frame_sub_analysis(buffer_samples, H, N)
@@ -56,7 +62,72 @@ def codec0(wavin, h, M, N):
         Z = frame_sub_synthesis(Yh, G)
     #Συσσώρευση σε xhat
         xhat[(bound1 *M):(bound2*M)] = Z
-    return xhat, Y_tot
+    # err = data - xhat
+    # plt.plot(err)
+    # plt.show()
+    #Write file to another file in our folder
+    wavfile.write("MYFILE_CODECO.wav", sr, xhat.astype(np.int16))
+    return xhat.astype(np.int16), Y_tot
+sr, x_data = wavfile.read('myfile.wav')
+x_hat, Y_tot = codec0('myfile.wav',h, M,N)
 
-x, y = codec0('myfile.wav',h, M,N)
-print(y)
+def signalPower(x):
+    return np.mean(np.square(x,dtype='int64')) #to prevent overflow
+
+
+def SNRsystem(inputSig, outputSig):
+    noise = outputSig - inputSig
+    powS = signalPower(outputSig)
+    powN = signalPower(noise)
+    return powS/powN
+
+#Experiments - SNR
+SNR = SNRsystem(x_data, x_hat)
+fig, axs = plt.subplots(2)
+fig.suptitle('Vertically stacked subplots')
+axs[0].plot(x_data[0:5000])
+axs[1].plot(x_hat[0:5000])
+plt.show()
+
+plt.plot(x_data[0:5000])
+plt.plot(x_hat[0:5000])
+plt.title("Comparison between the two bitstreams")
+plt.show()
+
+# print(SNR)
+
+
+#Coder only
+def coder0(wavin, h,M,N):
+    # Διαβάζω το αρχείο .wav
+    sr, data = wavfile.read(wavin)  # 514944
+    # Κατασκευάζω τα φίλτρα ανάλυσης
+    H = make_mp3_analysisfb(h, M)
+    # Δημιουργώ την παράμετρο για το l
+    L = len(h)  # 512
+    buffer_size = (N - 1) * M + L
+    # Υλοποιώ padding του σήματός μου για να μπορέσει να αναλυθεί σε subband
+    padding = np.zeros((L - M,))
+    iters = math.ceil(len(data) / (M * N))
+    data = np.append(data, padding)
+    # Αρχικοποιώ Y_tot, xhat
+    Y_tot = np.zeros((N * iters, M))
+    for i in range(0, iters):  # to kanw mia fora tha to kanw expand meta
+        # Διαβάζω το buffer: (N − 1)M + L
+        # Διαβάζω τα δείγματά μου: M*N
+        original_samples = data[(i * M * N):(i + 1) * M * N]
+        buffer_samples = data[(i * M * N):(i * M * N) + buffer_size]
+        # Frame Sub Analysis sto buffer mou
+        Y = frame_sub_analysis(buffer_samples, H, N)
+        # Επεξεργασία του frame
+        Yc = donothing(Y)
+        # Συσσώρευση
+        bound1 = i * N
+        bound2 = (i + 1) * N
+        Y_tot[bound1:bound2, :] = Yc
+    return Y_tot
+
+#Decoder Implementation
+def decoder0(Y_tot, h,M,N):
+    xhat = np.zeros(data.shape) #??????
+    return xhat
